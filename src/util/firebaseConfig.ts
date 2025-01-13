2780105590ea1ceee1707ee8a0b6ddb65fcf71b1
jamil4321 <jamil.ahmed.ja28@gmail.com>
2025-01-13 15:29:24 +0500

    update

--- src/util/firebaseConfig.ts
+++ src/util/firebaseConfig.ts
@@ -2,6 +2,8 @@
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 
+
+// firebase of client
 const firebaseConfig = {
     apiKey: "AIzaSyC8x_AvaE_ed6a69V9Ap4OWB4Cr6KzRHkg",
     authDomain: "mkcontracts-portal.firebaseapp.com",
@@ -10,7 +12,7 @@ const firebaseConfig = {
     messagingSenderId: "810403067933",
     appId: "1:810403067933:web:3b77fc9fe83e83da29eef4",
     measurementId: "G-X6MGNHQC96"
-  };
+};
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
