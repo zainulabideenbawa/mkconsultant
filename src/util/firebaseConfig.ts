ab6e88a5b6f17e08c7a34728d0216f593f6de677
jamil4321 <jamil.ahmed.ja28@gmail.com>
2025-01-13 15:25:22 +0500

    update firebase and db

--- src/util/firebaseConfig.ts
+++ src/util/firebaseConfig.ts
@@ -3,14 +3,14 @@ import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC4v9DBCSbHCwO3KZFhLEdIVNkk0PmpyG4",
-    authDomain: "mkconsultant-eee22.firebaseapp.com",
-    projectId: "mkconsultant-eee22",
-    storageBucket: "mkconsultant-eee22.appspot.com",
-    messagingSenderId: "87461473142",
-    appId: "1:87461473142:web:a8fcaf55b676962f5de857",
-    measurementId: "G-QXRY7TD3V9"
-};
+    apiKey: "AIzaSyC8x_AvaE_ed6a69V9Ap4OWB4Cr6KzRHkg",
+    authDomain: "mkcontracts-portal.firebaseapp.com",
+    projectId: "mkcontracts-portal",
+    storageBucket: "mkcontracts-portal.firebasestorage.app",
+    messagingSenderId: "810403067933",
+    appId: "1:810403067933:web:3b77fc9fe83e83da29eef4",
+    measurementId: "G-X6MGNHQC96"
+  };
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
